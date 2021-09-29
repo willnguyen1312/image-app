726fa9b5aaf2fb56989b94a59bf356efd2385a34
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-09-29 17:02:13 +0700

    Yeah Yeah

--- pages/layout-responsive.js
+++ pages/layout-responsive.js
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import ViewSource from '../components/view-source'
-import mountains from '../public/mountains.jpg'
+import Image from "next/image";
+import ViewSource from "../components/view-source";
+import mountains from "../public/mountains.jpg";
 
 const Responsive = () => (
   <div>
@@ -8,12 +8,13 @@ const Responsive = () => (
     <h1>Image Component With Layout Responsive</h1>
     <Image
       alt="Mountains"
-      src={mountains}
+      // src={mountains}
+      src="https://images.unsplash.com/photo-1632853199665-4afb5eaf2e46?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3387&q=80"
       layout="responsive"
       width={700}
       height={475}
     />
   </div>
-)
+);
 
-export default Responsive
+export default Responsive;
